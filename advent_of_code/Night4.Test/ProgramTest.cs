e450291670246d369135177be4a7a43d297aeec1
Lessley Dennington <oengdpswe@microsoft.com>
2020-12-07 13:18:24 -0800

    Cleaning up UTs

--- advent_of_code/Night4.Test/ProgramTest.cs
+++ advent_of_code/Night4.Test/ProgramTest.cs
@@ -8,40 +8,6 @@ namespace Night4.Test
     public class ProgramTest
     {
         [TestMethod]
-        public void Verify_Parse_Input()
-        {
-            string[] sampleInput = new string[] { "ecl:gry", "pid:860033327", "eyr:2020", "", "iyr:2013", "ecl:amb", "cid:350", "eyr:2023", "" };
-            List<Dictionary<string, string>> expected = new List<Dictionary<string, string>>()
-            {
-                new Dictionary<string, string>() {
-                    { "ecl", "gry" }, 
-                    { "pid", "860033327" },
-                    { "eyr", "2020" },
-                },
-                new Dictionary<string, string>() {
-                    { "iyr", "2013" },
-                    { "ecl", "amb" },
-                    { "cid", "350" },
-                    { "eyr", "2023" },
-                },
-            };
-            Program.ParseInput(sampleInput).Should().BeEquivalentTo(expected);
-        }
-
-        [DataTestMethod]
-        [DataRow("ecl:gry pid:860033327 eyr:2020")]
-        public void Verify_Create_Passport(string input)
-        {
-            Dictionary<string, string> expected = new Dictionary<string, string>() 
-            {
-                    { "ecl", "gry" },
-                    { "pid", "860033327" },
-                    { "eyr", "2020" },
-            };
-            Program.CreatePassport(input).Should().BeEquivalentTo(expected);
-        }
-
-        [TestMethod]
         public void Verify_Get_Valid_Passports()
         {
             List<Passport> input = new List<Passport>()
